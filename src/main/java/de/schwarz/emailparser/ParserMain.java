b244afece0059cb05e4f762486231f35f5707ad0
schwarz <schwarz@dipf.de>
2018-09-14 13:40:27 +0200

    Removed unused package imports.

--- src/main/java/de/schwarz/emailparser/ParserMain.java
+++ src/main/java/de/schwarz/emailparser/ParserMain.java
@@ -1,12 +1,5 @@
 package de.schwarz.emailparser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.net.InetAddress;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ParserMain {
     
