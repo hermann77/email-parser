e873b46c1f5a3d17dbe702b8a06dbe345c6be34d
Hermann Schwarz <tux1977@gmx.de>
2018-09-16 11:26:24 +0200

    Test: new test added * testInputIsNotNull - tests if Scanner object from input file given in args is NOT null.

--- src/test/java/de/schwarz/emailparser/ParserTest.java
+++ src/test/java/de/schwarz/emailparser/ParserTest.java
@@ -1,17 +1,33 @@
-
 package de.schwarz.emailparser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-
 import java.util.Scanner;
+import static org.junit.Assert.assertFalse;
+
 
 public class ParserTest {
-	
-    @Test public void testInputIsNotNull() {
+
+    /**
+     * tests if Scanner object from input file
+     * in resource folder (src/main/resources/emails_folder_file.txt) is NOT empty
+     */
+    @Test public void testInputFromResourceIsNotNull() {
         Parser parser = new Parser();
         Scanner input = parser.getInputFromResource();
         
+        assertFalse(input == null);
+    }
+
+    /**
+     * tests if Scanner object from input file
+     * given in args is NOT empty
+     *
+     * emails_folder_file.txt must be placed in project ROOT folder
+     */
+    @Test public void testInputIsNotNull() {
+        Parser parser = new Parser();
+        Scanner input = parser.getInputFromFileInArgs("emails_folder_file.txt");
+
         assertFalse(input == null);
     }
 }
