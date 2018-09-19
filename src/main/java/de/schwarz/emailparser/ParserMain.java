aa188f9268245bb34ae565911cb544e5f2abaa9e
schwarz <schwarz@dipf.de>
2018-09-19 14:38:57 +0200

    methode deleteEmailAddressesFromDB(String databaseName, String port) added. TODO: pass the port through AND test.

--- src/main/java/de/schwarz/emailparser/ParserMain.java
+++ src/main/java/de/schwarz/emailparser/ParserMain.java
@@ -29,34 +29,44 @@ public class ParserMain {
                 String inputFile = args[0];
                 input = parser.getInputFromFileInArgs(inputFile);
             }
-            // DATABASE selected/given
-            else if(args.length == 2) {
-                Connection connection = DBConnection.getConnection();
-                if(connection == null) {
-                    System.out.println("connection is null");
-                }
-
-                Statement stmt = null;
-                ResultSet resultSet = null;
-                try {
-                    stmt = connection.createStatement();
-                    resultSet = stmt.executeQuery("SELECT nl_email FROM lid_subscribers LIMIT 10");
-                    while (resultSet.next()) {
-                        String nl_email = resultSet.getString(1);
-                        System.out.print("DB e-mail address: " + nl_email);
-                    }
-                }
-                catch (SQLException e) {
-                    System.out.println("SQL query not executed: " + e.getErrorCode());
-                }
-
+            // DATABASE name passed in parameters 
+            else if(args.length > 1) {
+            	
+            	String databaseName = args[1];
+            	String port = args[2];
+            	deleteEmailAddressesFromDB(databaseName, port)
 
             }
         }
 
         parser.readLines(input);
 		
-	}	
+	}
+	
+	
+	
+	private void deleteEmailAddressesFromDB(String databaseName, String port) {
+		
+        Connection connection = DBConnection.getConnection();
+        if(connection == null) {
+            System.out.println("connection is null");
+        }
+
+        Statement stmt = null;
+        ResultSet resultSet = null;
+        try {
+            stmt = connection.createStatement();
+            resultSet = stmt.executeQuery("SELECT nl_email FROM lid_subscribers LIMIT 10");
+            while (resultSet.next()) {
+                String nl_email = resultSet.getString(1);
+                System.out.println("DB e-mail address: " + nl_email);
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("SQL query not executed: " + e.getErrorCode());
+        }
+
+	}
 	
 	
 }
